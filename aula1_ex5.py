762a550616cc9a9f19e60c5a52db5d672a4fdd95
miguelgallo <miguelgallo@users.noreply.github.com>
2019-03-29 01:04:11 -0300

    Add files via upload

---
+++ aula1_ex5.py
@@ -0,0 +1,8 @@
+print('Programa usado para calcular o índice de massa corporal')
+
+massa = 11
+altura = 70/100 # em m
+idade = 6/12 # em anos
+IMC = massa / ((altura)**2)
+
+print('O índice de massa corporal é: ', "%.3f" % IMC)
---
+++ aula1_ex6.py
@@ -0,0 +1,7 @@
+print ('Programa usado para calcular a velocidade final de um objeto em queda livre ')
+
+g = 9.8 #m/s^2
+h = 3 #metros
+v_final = (2*g*h)**(1/2)
+
+print('A velocidade final do objeto é: ', "%.3f" % v_final)
