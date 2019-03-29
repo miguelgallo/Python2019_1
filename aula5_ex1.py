df57efc82f2104548bc6b4f1aa6786db311825fa
miguelgallo <miguelgallo@users.noreply.github.com>
2019-03-29 11:34:33 -0300

    Add files via upload

---
+++ aula5_ex1.py
@@ -0,0 +1,12 @@
+import math
+
+print('Adicione três números para serem comparados: x1, x2, x3')
+x = eval(input('x: '))
+y = eval(input('y: '))
+z = eval(input('z: '))
+
+max(x, y, z)
+
+print('O máximo é: ', max(x, y, z))
+    
+
