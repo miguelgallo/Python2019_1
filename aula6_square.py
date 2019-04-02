5d6a1129e26b01bc1f7f3ad0dc68cedaffacd814
miguelgallo <miguelgallo@users.noreply.github.com>
2019-04-02 17:32:57 -0300

    Add files via upload

--- aula6_square.py
+++ aula6_square.py
@@ -8,22 +8,29 @@ bob = turtle.Turtle()                  # Criar Bob
 bob.shape("turtle")                    # Selecionar formato         
 bob.color("black")                     # Selecionar cor
 bob.pensize(5)                         # Selecionar largura       
-bob.speed("normal")                    # Selecionar velocidade
+bob.speed("fastest")                    # Selecionar velocidade
 
 print("Choose the lenght : ")  
 lenght = eval(input('Lenght: '))
 print("Choose the number of angles : ")  
 n_poly = eval(input('Number of angles: '))
-angle = 360/int(n_poly) 
+print("Choose the radius : ")  
+radius = eval(input('Radius: '))
 
 def square(t, l):
     for i in range(4):
         t.forward(l)               
         t.left(90)
 
-def polygon(t, l, a):
+def polygon(t, l, n_poly):
+    angle = 360/int(n_poly)
     for i in range(int(n_poly)):
         t.forward(l)                
-        t.left(a)
+        t.left(angle)
+
+def circle(t, r):
+     c = 2 * 3.1415 * r
+     l = c / 360
+     polygon(t, l, 360)
         
-polygon(bob, lenght, angle)
+circle(bob, radius)
