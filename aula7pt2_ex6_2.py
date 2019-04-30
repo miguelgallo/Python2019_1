8c69146bd8217e451ca49ad5d7f9ca38d667cceb
miguelgallo <miguelgallo@users.noreply.github.com>
2019-04-30 15:24:55 -0300

    Add files via upload

---
+++ aula7pt2_ex6_2.py
@@ -0,0 +1,26 @@
+import turtle
+bob = turtle.Turtle()
+
+def koch(t, l):
+    # t: turtle
+    # l: length
+    if l < 3:
+        t.fd(l)
+        return
+    koch(t,l/3)
+    t.lt(60)
+    koch(t,l/3)
+    t.rt(120)
+    koch(t,l/3)
+    t.lt(60)
+    koch(t,l/3)
+	
+#inner angle summation is 360 degrees
+def snowflake(t, l):
+    koch(t, l)for i in range(3):
+        koch(t, n)
+        t.rt(120)
+
+snowflake(bob, 300)
+
+turtle.mainloop()
