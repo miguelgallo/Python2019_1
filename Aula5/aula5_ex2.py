8cf2446e9ad7c5de97af2b808d7e61a8be879977
miguelgallo <miguelgallo@users.noreply.github.com>
2019-05-23 23:13:02 -0300

    Add files via upload

---
+++ Aula5/aula5_ex2.py
@@ -0,0 +1,7 @@
+import math
+
+a = [1, 2, 3, 4, 5]
+
+b = sum(a)
+
+print(b)
---
+++ Aula5/aula5_ex5.py
@@ -0,0 +1,7 @@
+import math
+
+lista = [10, 21, 353, 4, 256, 45, 66, 93] 
+   
+for num in lista:  
+    if num % 2 == 0: 
+        print(num) 
