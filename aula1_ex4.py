2aa79f56bc590913c62c55acfdc793d89caab7ef
miguelgallo <miguelgallo@users.noreply.github.com>
2019-05-23 23:19:22 -0300

    Delete aula1_ex4.py

--- aula1_ex4.py
+++
@@ -1,7 +0,0 @@
-import math
-
-tan = 0.5 / 5
-resultado = math.atan(tan)
-graus = math.degrees(resultado)
-
-print ('O valor do ângulo zenital do sol é: ', "%.2f" % resultado, 'radianos ou ', "%.2f" % graus , 'graus')
