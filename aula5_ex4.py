70760f7451a54f50ef74179d6bc878269f10af45
miguelgallo <miguelgallo@users.noreply.github.com>
2019-05-24 00:29:43 -0300

    Delete aula5_ex4.py

--- aula5_ex4.py
+++
@@ -1,9 +0,0 @@
-def test_range(n):
-    if n in range(0,99):
-        print( " %s está no intervalo "%str(n))
-    else :
-        print("O número está fora do intervalo")
-
-n = eval(input('Adicione o número a ser testado: '))
-
-test_range(n)
