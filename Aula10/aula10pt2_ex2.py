be9ee2406b0861e473580fa25691ca4da948ab4e
miguelgallo <miguelgallo@users.noreply.github.com>
2019-06-04 19:49:35 -0300

    Add files via upload

--- Aula10/aula10pt2_ex2.py
+++ Aula10/aula10pt2_ex2.py
@@ -10,10 +10,14 @@ a = 20.25 #acelaração
 tac = [i for i in range(0,10)]  #intervalos de 1 minuto, 9 minutos (onde ocorre a aceleração)
 dac = [d0+v0/60 * ti +(((a/3600) * ti**2)/2) for ti in tac]
 d1 = dac[9]
-t = tac + [i for i in range(10,30)]  #intervalos de 1 minuto, indo de 10 a 30 min (onde não há aceleração)
-d = dac[0:8] + [d1+v1/60 * tin for tin in t]
+t2 = [i for i in range(10,30)] #intervalos de 1 minuto, indo de 10 a 30 min (onde não há aceleração)
+t = tac + t2
+d = dac[0:8] + [d1+v1/60 * tin for tin in t2]
 
+print(d1)
 print(d)
+print(len(d))
+print(len(t))
 
 plt.plot(t,d)
 plt.show()
