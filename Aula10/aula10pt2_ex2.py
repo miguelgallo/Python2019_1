c8974b39b1cda5a7a2d6aa5a9b8c31edcd115919
miguelgallo <miguelgallo@users.noreply.github.com>
2019-06-05 14:46:43 -0300

    Add files via upload

--- Aula10/aula10pt2_ex2.py
+++ Aula10/aula10pt2_ex2.py
@@ -1,25 +1,28 @@
 import matplotlib.pyplot as plt 
 
-d0 = 0    #distancia inicial
-dac = 2   #distancia percorrida com aceleração
+d0 = 5    #distancia inicial
+dac = 5.2   #distancia percorrida com aceleração
 dfin = 7  #distancia final
 v0 = 12   #velocidade inicial
 v1 = 15   #velocidade após acelerar 
-a = 20.25 #acelaração
+a = 202.5 #acelaração
 
-tac = [i for i in range(0,10)]  #intervalos de 1 minuto, 9 minutos (onde ocorre a aceleração)
-dac = [d0+v0/60 * ti +(((a/3600) * ti**2)/2) for ti in tac]
-d1 = dac[9]
-t2 = [i for i in range(10,30)] #intervalos de 1 minuto, indo de 10 a 30 min (onde não há aceleração)
-t = tac + t2
-d = dac[0:8] + [d1+v1/60 * tin for tin in t2]
+tac = [i for i in range(0,2)]  #intervalos de 1 minuto (onde ocorre a aceleração)
+dac = [d0 + (v0/60)*ti + (((a/3600)*(ti**2))/2) for ti in tac]
+t2 = [i for i in range(2,30)]  #intervalos de 1 minutos, entre 1 a 30min (onde não tem mais acelaração)
+t = tac + [i for i in range(2,30)]
+d = dac + [dac[1] + (v1/60)*tin for tin in t2]
 
-print(d1)
+print(tac)
+print(dac)
+print(t)
 print(d)
-print(len(d))
 print(len(t))
+print(len(d))
 
 plt.plot(t,d)
+plt.xlabel("tempo(min)")
+plt.ylabel("distancia(km)")
 plt.show()
 
 #i = d.index(7.0278125)
